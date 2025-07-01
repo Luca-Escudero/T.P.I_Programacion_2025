702a56d442507bd89d4aebce8d222f86cf57391e
Luca <luca.escudero2.0@gmail.com>
2025-07-01 18:34:56 -0300

    Contructores, getters y setters

--- CopaManager/src/Modelos/Entrenador.java
+++ CopaManager/src/Modelos/Entrenador.java
@@ -1,5 +1,52 @@
 package modelos;
 
 public class Entrenador {
+    private int idEntrenador;
+    private String nombre;
+    private int edad;
+    private String nacionalidad;
+
+    public Entrenador(int idEntrenador, String nombre, int edad, String nacionalidad) {
+        this.idEntrenador = idEntrenador;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.nacionalidad = nacionalidad;
+    }
+
+    public int getIdEntrenador() {
+        return idEntrenador;
+    }
+
+    public void setIdEntrenador(int idEntrenador) {
+        this.idEntrenador = idEntrenador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    
+
+    
 
 }
